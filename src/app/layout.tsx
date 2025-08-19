beb8750ed881be03b16e0f0d239dcc5da41495e1
Randy <randysim9@gmail.com>
2025-08-19 14:11:13 -0400

    feat: sessionprovider setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { getServerSession } from "next-auth";
+import { options } from "@/app/api/auth/[...nextauth]/route";
+import SessionProvider from "@/app/components/SessionProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,17 +20,21 @@ export const metadata: Metadata = {
   description: "Yet another Next.js SaaS boilerplate",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(options);
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SessionProvider session={session}>
+          {children}
+        </SessionProvider>
       </body>
     </html>
   );
