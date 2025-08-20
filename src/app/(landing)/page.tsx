9ebaef2c88e0983ca66ecc90335c7c0817b0aad4
Randy <randysim9@gmail.com>
2025-08-19 20:21:24 -0400

    feat: hero text

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,24 +1,16 @@
-import { auth, signIn } from "@/auth";
-
 export default async function Home() {
-  const session = await auth();
-
   return (
     <div>
-      <h1>Yansb</h1>
-      <p>
-        {session ? `Signed in as ${session.user?.email}` : "Not signed in"}
-      </p>
-
-      <form
-        action={async () => {
-          "use server"
-          await signIn()
-        }}
-      >
-        <button type="submit">Sign In</button>
-      </form>
-      
+      {/* HERO SECTION */}
+      <section className="flex flex-col items-center justify-center h-screen text-center">
+        <h1 className="font-bold text-4xl max-w-xl p-5">The optimized and opinionated Next.js SaaS template.</h1>
+        <h4 className="max-w-2xl">
+          Don't waste time re-inventing the wheel, 
+          Yasnb has your MVP covered. Stop learning
+          Next.js best practices, start shipping
+          code to production.
+        </h4>
+      </section>
     </div>
   );
 }
