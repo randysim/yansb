c3a3a25b8cd41904be345682b3bdd1db6296e861
Randy <randysim9@gmail.com>
2025-08-19 22:55:28 -0400

    feat: fix hero

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -2,9 +2,9 @@ export default async function Home() {
   return (
     <div>
       {/* HERO SECTION */}
-      <section className="flex flex-col items-center justify-center h-screen text-center">
-        <h1 className="font-bold text-4xl max-w-xl p-5">The optimized and opinionated Next.js SaaS template.</h1>
-        <h4 className="max-w-2xl">
+      <section className="flex flex-col items-center justify-center h-[550px] text-center">
+        <h1 className="font-bold text-6xl max-w-3xl p-5">Think less. Build more.</h1>
+        <h4 className="max-w-2xl text-lg">
           Don't waste time re-inventing the wheel, 
           YANSB has your MVP covered. Stop learning
           Next.js best practices, start shipping
