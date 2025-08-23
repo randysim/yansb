740d441fa0699f73f6c007789c318c91f5508b48
Randy <randysim9@gmail.com>
2025-08-23 14:35:00 -0400

    feat: temp dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { auth } from "@/auth";
 import db from "@/db";
 import { settings } from "@/db/schema";
 import { eq } from "drizzle-orm";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 
 export default async function DashboardPage() {
@@ -21,9 +22,16 @@ export default async function DashboardPage() {
   }
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome to your dashboard!</p>
+    <div className="flex justify-center items-center py-24">
+      <Card className="mx-4">
+        <CardHeader className="text-center">
+          <h1 className="text-4xl font-semi-bold">Dashboard</h1>
+          <p className="mt-4 text-lg">Welcome back {session.user.name}!</p>
+        </CardHeader>
+        <CardContent>
+          <p>This is where your dashboard goes! Add the main page for your dashboard.</p>
+        </CardContent>
+      </Card>
     </div>
   );
 }
